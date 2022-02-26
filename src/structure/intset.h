a6ff7a503c25d5147e7a2db47e0ffd4239a5871a
Eric Allen <1213489891@qq.com>
2022-02-26 21:51:51 +0800

    增加了整数集合的定义

---
+++ src/structure/intset.h
@@ -0,0 +1,21 @@
+//
+// Created by Administrator on 2022/2/26.
+//
+
+#ifndef REDIS_DESIGN_INTSET_H
+#define REDIS_DESIGN_INTSET_H
+
+#include <stdint.h>
+
+/**
+ * 用于保存整数值的整数集合，保存类型为int16_t、int32_t、int64_t的整数值，
+ * 并保证不会出现重复元素。
+ */
+typedef struct intset
+{
+    uint32_t encoding;  //编码方式
+    uint32_t length;    //集合包含的元素数量
+    int8_t contents[];  //保存元素的数组
+} intset;
+
+#endif //REDIS_DESIGN_INTSET_H
