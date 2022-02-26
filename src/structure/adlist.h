da1b185d097676b449bbdee10f2c1f4122330c6a
Eric Allen <1213489891@qq.com>
2022-02-26 15:55:03 +0800

    链表和链表节点的实现

---
+++ src/structure/adlist.h
@@ -0,0 +1,10 @@
+//
+// Created by Administrator on 2022/2/26.
+//
+
+#ifndef REDIS_DESIGN_ADLIST_H
+#define REDIS_DESIGN_ADLIST_H
+
+
+
+#endif //REDIS_DESIGN_ADLIST_H
--- src/structure/sds.h
+++ src/structure/sds.h
@@ -5,6 +5,7 @@
  * 用于动态表示字符串的结构声明。
  * 简单动态字符串(Simple Dynamic Strings, SDS)
  * 存储字符串和整型数据
+ * 整个结构分配的字节数等于len+free+1(\0)
  */
 #ifndef REDIS_DESIGN_SDS_H
 #define REDIS_DESIGN_SDS_H
@@ -18,5 +19,14 @@ struct sds
     //字节数组，用于保存字符串
     char buf[];
 };
+
+struct _attribute_((_packed_))sdshdr5
+{
+    //低3位存储类型，高5位存储长度
+    unsigned char flags;
+    char buf[];
+};
+
+
 
 #endif //REDIS_DESIGN_SDS_H
